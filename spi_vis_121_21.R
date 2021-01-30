c46666a847e8ebca16976ac901fb027f8d783338
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2021-01-30 15:03:01 -0500

    Delete spi_vis_121_21.R

--- spi_vis_121_21.R
+++
@@ -1,8 +0,0 @@
-url <- 'https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv'
-df <- read.csv(url, header=TRUE)
-df <- na.omit(df)
-attach(df)
-spi_margin = abs(spi1 - spi2)
-margin = abs(score1 - score2)
-plot(margin ~ spi_margin, col=rainbow(4))
-detach()
