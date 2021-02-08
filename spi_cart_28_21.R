0620eee33a22260e5ca2658b8e14bfdc524278ef
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2021-02-08 12:15:48 -0500

    Add files via upload

--- spi_cart_28_21.R
+++ spi_cart_28_21.R
@@ -5,14 +5,16 @@ url <- 'https://projects.fivethirtyeight.com/soccer-api/club/spi_matches.csv'
 df <- read.csv(url) %>% na.omit()
 bpl <- df[df$league=='Barclays Premier League',]
 gb <- df[df$league=='German Bundesliga',]
+df$date <- as.factor(df$date)
 
 df <- rbind(bpl, gb)
 df$league <- as.factor(df$league)
+df <- df[,3:ncol(df)]
 
 idx <- createDataPartition(df$league, p=0.8, list=FALSE)
 test <- df[-idx,]
 train <- df[idx,]
 
-cart <- train(league ~ ., data=df, method="rpart", metric="Accuracy", trControl=trainControl(method="repeatedcv", number=10, repeats=3))
+cart <- train(league ~ ., data=train, method="rpart", metric="Accuracy", trControl=trainControl(method="repeatedcv", number=10, repeats=3))
 predictions <- predict(cart, test)
 confusionMatrix(predictions, test$league)
