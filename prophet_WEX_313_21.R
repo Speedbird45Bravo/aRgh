776b6856b162c4f692f02cfe3ac01e0494816078
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2021-03-13 00:12:46 -0500

    Add files via upload

---
+++ prophet_WEX_313_21.R
@@ -0,0 +1,17 @@
+library(BatchGetSymbols)
+library(prophet)
+library(dplyr)
+
+start <- as.Date("2019-01-01")
+end <- as.Date("2021-03-13")
+
+df <- BatchGetSymbols("WEX", first.date=start, last.date=end) %>% as.data.frame()
+
+df <- data.frame(df$df.tickers.ref.date, df$df.tickers.price.adjusted)
+names(df) <- c("ds", "y")
+
+model <- prophet(df)
+future <- make_future_dataframe(model, periods=365)
+forecast <- predict(model, future)
+
+plot(model, forecast, xlab="Date", ylab="$USD")
